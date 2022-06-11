eb5a21735a2672e2a9b9e56f3ebf68e6f37ca60a
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2022-06-11 14:11:13 +0530

    filed deleted

--- schemas/schema.js
+++ schemas/schema.js
@@ -2,7 +2,6 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 import seo from "./components/others/seo";
-import ads from "./components/affiliate-ads/ads";
 import shop from "./components/shop/shop";
 import blog from "./components/blogs/blog";
 import offer from "./components/shop/offer";
@@ -27,7 +26,6 @@ export default createSchema({
     travelCategory,
     travelPosts,
     comments,
-    ads,
     productCategory,
     offer,
     shop,
